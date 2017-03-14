3af3c8acc59926d229d784f2828b737d1da6cb7c
Netopya <netopya@netopyaplanet.com>
2017-03-14 14:58:30 -0400

    Added symbol table classes

---
+++ COMP442-Assignment3/SymbolTable/Entry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP442_Assignment3.SymbolTable
+{
+    public enum EntryKinds
+    {
+        function,
+        classKind,
+        parameter,
+        variable
+    };
+
+    public abstract class Entry
+    {
+        bool declared;
+
+        SymbolTable child;
+        SymbolTable parent;
+        EntryKinds kind;
+
+        public Entry(SymbolTable parent, EntryKinds kind)
+        {
+            this.parent = parent;
+            this.kind = kind;
+        }
+
+    }
+}
---
+++ COMP442-Assignment3/SymbolTable/SymbolTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP442_Assignment3.SymbolTable
+{
+    public class SymbolTable
+    {
+        string name;
+        Entry parent;
+        List<Entry> entries = new List<Entry>();
+    }
+}
